c833ebcfb5ef4c9158ce7e944717bcf6c2214ea9
getbackwards <getbackwards@gmail.com>
2018-01-14 17:53:25 +1300

    pushing

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -25,6 +25,16 @@ var myPromise = new Promise(function (resolve, reject) {
     }, 3000);
 });
 
+var xhr = new XMLHttpRequest();
+xhr.open('GET', 'https://httpbin.org/ip');
+xhr.responseType = 'json';
+
+xhr.onload = function () {
+    console.log('Error');
+};
+
+xhr.send();
+
 fetch('https://httpbin.org/ip')
     .then(function (response) {
         console.log(response);
@@ -43,6 +53,7 @@ fetch('https://httpbin.org/post', {
         'Content-Type': 'application/json',
         'Accept': 'applications/json'
     },
+    mode: 'cors',
     body: JSON.stringify({message: 'Does this work?'})
 })
     .then(function (response) {
